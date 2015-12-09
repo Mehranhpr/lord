17a1e7e6fe4fc77d3b6c4f98c77bb322d2c52344
Mehranhpr <mehranhpr@icloud.com>
2015-12-09 16:09:49 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {128556958(our_id)},--Sudo users
+    sudo_users = {0,128556958(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
